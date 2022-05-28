6595c51d81489614581e3b8782c0aa313c213256
akashkmt <akashkmt963@gmail.com>
2022-05-28 16:43:12 +0530

    done

--- scripts/edit.js
+++ scripts/edit.js
@@ -1,14 +1,12 @@
 var id = (localStorage.getItem('taskId'));
-getData();
+showData();
 function updateData (){
         var input = document.getElementById('taskDetails').value;
         var checkedValue = document.getElementById('taskStatus').checked;
-        // console.log(input, checkedValue);
         var object = {
             task: input,
             status: checkedValue
         }
-        // console.log(object);
         if(input == ''){
             alert('Please enter a task');
         }
@@ -19,7 +17,7 @@ function updateData (){
     
     async function updateDataBase(data){
         try {
-            let upadate = await fetch(`http://localhost:3000/allTasks/${id}`, {
+            let update = await fetch(`http://localhost:3000/allTasks/${id}`, {
                 method: 'PUT',
                 body: JSON.stringify(data),
                 headers: { "Content-Type": "application/json" }
@@ -30,11 +28,10 @@ function updateData (){
         }
     }
 
-    async function getData(){
+    async function showData(){
         try {
             let get = await fetch(`http://localhost:3000/allTasks/${id}`);
             let data = await get.json();
-            // console.log(data);
             document.getElementById('taskDetails').value = data.task;
             document.getElementById('taskStatus').checked = data.status;
         } catch (error) {
